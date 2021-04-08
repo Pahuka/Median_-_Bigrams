a0a55c0f5a1fe35ae604d763f088518eca3c6d5b
Pahuka <J-a-c-k@yandex.ru>
2021-04-08 15:59:36 +0500

    Реализовал ParseSlideRecords

--- ParsingTask.cs
+++ ParsingTask.cs
@@ -11,15 +11,27 @@ namespace linq_slideviews
 		/// <remarks>Метод должен пропускать некорректные строки, игнорируя их</remarks>
 		public static IDictionary<int, SlideRecord> ParseSlideRecords(IEnumerable<string> lines)
 		{
-			throw new NotImplementedException();
+            if (lines.Count() > 1)
+                return lines
+                .Skip(1)
+                .Select(x => x.Split(';'))
+                .ToDictionary(x => int.Parse(x[0]), x => new SlideRecord(int.Parse(x[0]), (SlideType)FindSlideType(x[1]), x[2]));
+
+            return new Dictionary<int, SlideRecord>();
 		}
 
-		/// <param name="lines">все строки файла, которые нужно распарсить. Первая строка — заголовочная.</param>
-		/// <param name="slides">Словарь информации о слайдах по идентификатору слайда. 
-		/// Такой словарь можно получить методом ParseSlideRecords</param>
-		/// <returns>Список информации о посещениях</returns>
-		/// <exception cref="FormatException">Если среди строк есть некорректные</exception>
-		public static IEnumerable<VisitRecord> ParseVisitRecords(
+        private static int FindSlideType(string type)
+        {
+            List<string> list = Enum.GetNames(typeof(SlideType)).Select(x => x.ToLower()).ToList();
+            return list.IndexOf(type);
+        }
+
+        /// <param name="lines">все строки файла, которые нужно распарсить. Первая строка — заголовочная.</param>
+        /// <param name="slides">Словарь информации о слайдах по идентификатору слайда. 
+        /// Такой словарь можно получить методом ParseSlideRecords</param>
+        /// <returns>Список информации о посещениях</returns>
+        /// <exception cref="FormatException">Если среди строк есть некорректные</exception>
+        public static IEnumerable<VisitRecord> ParseVisitRecords(
 			IEnumerable<string> lines, IDictionary<int, SlideRecord> slides)
 		{
 			throw new NotImplementedException();
