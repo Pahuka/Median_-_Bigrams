0cf908d09761cc215cb8e1343858fe8e09a2b209
Pahuka <J-a-c-k@yandex.ru>
2021-04-14 12:36:09 +0500

    Исправляю ошибки в методе GetMedianTimePerSlide

--- StatisticsTask.cs
+++ StatisticsTask.cs
@@ -8,19 +8,24 @@ namespace linq_slideviews
     {
         public static double GetMedianTimePerSlide(List<VisitRecord> visits, SlideType slideType)
         {
-            var temp = DateTime.Now - TimeSpan.FromMinutes(40);
-            if (visits.Count > 1)
-            {
-                var t = visits
-                    .GroupBy(userId => userId.UserId)
-                    .Where(key => key.Select(time => time.DateTime) > TimeSpan.FromMinutes(1));
+            var result = new List<double>();
+            var tempValue = visits
+                .GroupBy(userId => userId.UserId)
+                .Select(x => x.Bigrams())
+                .Select(x => x.Where(y => y.Item1.SlideType == slideType && y.Item2.SlideType == slideType))
+                .SelectMany(x => x.Where(y => (y.Item2.DateTime - y.Item1.DateTime) >= TimeSpan.FromMinutes(1)
+                || (y.Item2.DateTime - y.Item1.DateTime) <= TimeSpan.FromHours(2)));
+            //.Select(x => (double)x.Item2.DateTime.Minute - x.Item1.DateTime.Minute);
 
-                //return visits
-                //    .GroupBy(userId => userId.UserId)
-                //    .Select(time => time.Select(c => (double)c.DateTime.Minute).Bigrams())
-                //    .Select(x => x.Select(y => y.Item2 - y.Item1))
-                //    .SelectMany(x => x)
-                //    .Median();
+            if (tempValue.Count() != 0)
+            {
+                foreach (var item in tempValue)
+                {
+                    var minutes = item.Item2.DateTime - item.Item1.DateTime;
+                    result.Add(minutes.Minutes);
+                }
+
+                return result.Median();
             }
 
             return 0;
